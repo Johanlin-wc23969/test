10b9391bdf2b6bf824a3bfd2524d4a5b966fbd1d
Johan Lin <157592191+Johanlin-wc23969@users.noreply.github.com>
2024-10-03 21:49:59 +0100

    Update testServer.go

--- net/server/testServer.go
+++ net/server/testServer.go
@@ -1,29 +1,28 @@
-// package main
+ package main
 
-// import (
-// 	"bufio"
-// 	"fmt"
-// 	"net"
-// )
+ import (
+ 	"bufio"
+ 	"fmt"
+ 	"net"
+ )
 
-// func handleconnection(conn net.Conn) {
-// 	//read msg from client
-// 	for {
-// 		reader, _ := bufio.NewReader(conn).ReadString('\n')
-// 		fmt.Print("message from client: ")
-// 		fmt.Println(reader)
-// 		//send msg ok back to client
-// 		fmt.Fprintln(conn, "ok")
-// 	}
-// }
+ func handleconnection(conn net.Conn) {
+ 	//read msg from client
+ 	for {
+ 		reader, _ := bufio.NewReader(conn).ReadString('\n')
+ 		fmt.Print("message from client: ")
+ 		fmt.Println(reader)
+ 		//send msg ok back to client
+ 		fmt.Fprintln(conn, "ok")
+ 	}
+ }
+ func main() {
+ 	ln, _ := net.Listen("tcp", ":8080")
+ 	for {
+ 		conn, _ := ln.Accept()
+ 		fmt.Println("Successfully connected with client!")
+ 		fmt.Println("Waiting for message...")
+ 		go handleconnection(conn)
 
-// func main() {
-// 	ln, _ := net.Listen("tcp", ":8080")
-// 	for {
-// 		conn, _ := ln.Accept()
-// 		fmt.Println("Successfully connected with client!")
-// 		fmt.Println("Waiting for message...")
-// 		go handleconnection(conn)
-
-// 	}
-// }
+ 	}
+ }
