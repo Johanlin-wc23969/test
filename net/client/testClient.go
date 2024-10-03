7f6f78c5a43317eb95f4464b52adaedc7e89fe1e
Johan Lin <157592191+Johanlin-wc23969@users.noreply.github.com>
2024-10-03 21:49:11 +0100

    Update testClient.go

--- net/client/testClient.go
+++ net/client/testClient.go
@@ -1,26 +1,26 @@
-// package main
+ package main
 
-// import (
-// 	"bufio"
-// 	"fmt"
-// 	"net"
-// 	"os"
-// )
+ import (
+ 	"bufio"
+ 	"fmt"
+ 	"net"
+ 	"os"
+ )
 
-// func readMessage(conn net.Conn) {
-// 	msg, _ := bufio.NewReader(conn).ReadString('\n')
-// 	fmt.Println(msg)
-// }
-// func main() {
-// 	conn, _ := net.Dial("tcp", "127.0.0.1:8080")
-// 	stdin := bufio.NewReader(os.Stdin)
-// 	for {
-// 		//read input from terminal and send msg to server
-// 		fmt.Print("Enter text: ")
-// 		msg, _ := stdin.ReadString('\n')
-// 		fmt.Fprintln(conn, msg)
-// 		//reveive msg from server
-// 		fmt.Print("response from server: ")
-// 		readMessage(conn)
-// 	}
-// }
+ func readMessage(conn net.Conn) {
+ 	msg, _ := bufio.NewReader(conn).ReadString('\n')
+ 	fmt.Println(msg)
+ }
+ func main() {
+ 	conn, _ := net.Dial("tcp", "127.0.0.1:8080")
+ 	stdin := bufio.NewReader(os.Stdin)
+ 	for {
+ 		//read input from terminal and send msg to server
+ 		fmt.Print("Enter text: ")
+ 		msg, _ := stdin.ReadString('\n')
+ 		fmt.Fprintln(conn, msg)
+ 		//reveive msg from server
+ 		fmt.Print("response from server: ")
+ 		readMessage(conn)
+ 	}
+ }
